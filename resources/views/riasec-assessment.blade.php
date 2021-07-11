305780ff6fc551905846ba2e731fda9902759c2c
Jessica Aurellia <jessrellia@gmail.com>
2021-07-11 18:01:22 +0700

    RIASEC Question blade

--- resources/views/riasec-assessment.blade.php
+++ resources/views/riasec-assessment.blade.php
@@ -6,8 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
-    Hello, my love.
+        <form action="/get-riasec-data" method="POST">
+        {{csrf_field()}}
+        @foreach($questions as $question)
+            <div class='form-check'>
+                <input class="form-check-input" type="checkbox" value="{{$question->code}}" name="checked[]" id="question-check">
+                <label class="form-check-label" for="question-check">
+                    {{$question->id}}
+                    {{$question->question}}
+                    {{$question->code}}
+                </label> 
+            </div>  
+        @endforeach
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>   
 </body>
 </html>
