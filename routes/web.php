50bc15c842a9ee94c0643e85a1b1c258b20aa916
Jessica Aurellia <jessrellia@gmail.com>
2021-07-11 12:42:53 +0700

    Route for RIASEC assessment

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/riasec-assessment', 'AssessmentController@view_riasec_assessment');
