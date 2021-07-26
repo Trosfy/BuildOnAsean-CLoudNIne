fd9bd8508806595cbbb939d91b5da422fdb3c484
Jessica Aurellia <jessrellia@gmail.com>
2021-07-26 15:06:38 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::post('/major-assessment/3', 'AssessmentController@majorAssessment3save');
 Route::get('/career-assessment', 'AssessmentController@careerAssessment');
 Route::get('/career-assessment-q', 'AssessmentController@careerAssessmentQ');
 Route::get('/career-assessment-result', 'AssessmentController@viewCareerResult');
+Route::post('career-assessment-q', 'AssessmentController@careerAssessmentSave'); 
 
 // Careers
 Route::get('/careers', 'CareerController@showAll');
