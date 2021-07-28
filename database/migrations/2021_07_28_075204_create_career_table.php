215d909d89c126021c84d9fb457830faa52dd8db
Jessica Aurellia <jessrellia@gmail.com>
2021-07-28 15:03:15 +0700

    Career table

---
+++ database/migrations/2021_07_28_075204_create_career_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCareerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('career', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('jobtitle');
+            $table->unsignedInteger('major_id');
+            $table->foreign('major_id')->references('id')->on('majors')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('jurusansma'); 
+            $table->boolean('r');
+            $table->boolean('i');
+            $table->boolean('a');
+            $table->boolean('s');
+            $table->boolean('e');
+            $table->boolean('c');
+            $table->string('overview');
+            $table->string('jobdesc');
+            $table->longText('basicskill');
+            $table->longText('socialskill');
+            $table->longText('peopleskill');
+            $table->longText('personality'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('career');
+    }
+}
