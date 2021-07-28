75e45007b852a1b7de26bb67d2fab2aeb64b371d
Jessica Aurellia <jessrellia@gmail.com>
2021-07-28 18:24:45 +0700

    Updated major controller

--- app/Http/Controllers/MajorController.php
+++ app/Http/Controllers/MajorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MajorController extends Controller
 {
@@ -45,8 +46,10 @@ class MajorController extends Controller
      */
 
     public function showAll(){
-        
-        return view('major.majors-list');
+
+        $majors = DB::table('uni_majors')->join('majors', 'majors.id', '=', 'uni_majors.major_id')->join('universities', 'universities.id', '=', 'uni_majors.university_id')->join('careers', 'careers.major_id', '=', 'majors.id')->select('universities.name AS uni_name', 'careers.jobtitle', 'careers.overview', 'uni_majors.budget', 'careers.jurusansma')->get(); 
+
+        return view('major.majors-list', ['majors' => $majors]);
     }
 
     public function show($id)
