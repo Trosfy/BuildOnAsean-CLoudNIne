f9a6df1554f1544e09ae39767e5cbcf56be2d3e5
Trosfy <61226342+Trosfy@users.noreply.github.com>
2021-07-28 21:50:37 +0700

    update route abis register

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PageController@landing');
 Route::get('/about', 'PageController@about');
 Route::get('/search', 'PageController@search');
+Route::get('/home', function(){
+    return redirect('/');
+});
 // Route::get('/dashboard', 'PageController@dashboard');
 
 Route::middleware(['auth'])->group(function(){
